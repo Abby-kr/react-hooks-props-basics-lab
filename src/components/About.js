042f159757cfccb2ea13c528c60f8d1e8d58915f
Abby-kr <abby.a.otieno@gmail.com>
2023-08-27 21:21:53 +0300

    Completed Lab

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,20 @@
 import React from "react";
+import user from "../data/user";
+import Links from "./Links";
 
-function About() {
+function About(props) {
+ if (!props.bio || props.bio.length === 0){
+  return null
+ }
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+        <p>{props.bio}</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links links={user.links}/>
     </div>
   );
 }
+
 
 export default About;
--- src/components/App.js
+++ src/components/App.js
@@ -8,11 +8,12 @@ import user from "../data/user";
 console.log(user);
 
 function App() {
+   
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home color={user.color} name={user.name} city={user.city}/>
+      <About bio = {user.bio} links={user.links}/>
     </div>
   );
 }
