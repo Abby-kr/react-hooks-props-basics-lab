ba35f7597a00c31f9ff5173d7e5c8ce3fa27043f
Abby-kr <abby.a.otieno@gmail.com>
2023-09-06 12:08:41 +0300

    Resubmission of Lab

--- src/components/About.js
+++ src/components/About.js
@@ -11,7 +11,7 @@ function About(props) {
       <h2>About Me</h2>
         <p>{props.bio}</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      <Links links={user.links}/>
+      <Links github={props.github} linkedin={props.linkedin}/>
     </div>
   );
 }
--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,6 @@ import About from "./About";
 
 // pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
-console.log(user);
 
 function App() {
    
@@ -13,7 +12,7 @@ function App() {
     <div>
       <NavBar />
       <Home color={user.color} name={user.name} city={user.city}/>
-      <About bio = {user.bio} links={user.links}/>
+      <About bio = {user.bio} github={user.links.github} linkedin={user.links.linkedin}/>
     </div>
   );
 }
